4e72a32b89a9e9dd8cc5cd91f4e60c85c56e146c
chenhuifei01 <chenhui1@uchicago.edu>
2024-04-15 19:08:06 -0500

    completed clone_repo function

--- src/cocoa/repo.py
+++ src/cocoa/repo.py
@@ -3,6 +3,10 @@ Utilities for evaluating the status and structure of a git repository
 """
 import git
 
+import os
+import tempfile
+from git import Repo, GitCommandError
+
 
 def is_git_repo(repo_path):
     """
@@ -67,3 +71,47 @@ def get_current_branch(repo_path):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     return None
+
+
+def clone_repo(repo_url, dir_path="temp_repo_dir"):
+    """
+    Clones a Git repository into a specified directory, or updates it if it already exists.
+
+    Parameters:
+    - repo_url (str): The URL of the repository to be cloned. This must not be empty.
+    - dir_path (str): The local directory path where the repository should be cloned.
+                      Default is "temp_repo_dir".
+
+    Returns:
+    - str: The path to the cloned repository.
+    """
+    if not repo_url:
+        raise ValueError("Repository URL must be provided.")
+
+    try:
+        # check the dir path exist
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        print(f"Cloning {repo_url} into {dir_path}")
+        repo_path = os.path.join(dir_path, repo_url.split('/')[-1])
+
+        # update but not clone if the dir existed
+        if os.path.exists(repo_path):
+            print(
+                f"The directory {repo_path} already exists. Fetching the latest changes.")
+            repo = Repo(repo_path)
+            repo.remote().fetch()
+        else:
+            repo = Repo.clone_from(repo_url, repo_path)
+
+        # get all branches
+        repo.git.fetch('--all')
+
+        return repo_path
+    except GitCommandError as e:
+        print(f"Error during git operation: {e}")
+        raise
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        raise
