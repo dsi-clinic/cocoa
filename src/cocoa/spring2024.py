959bc7064b06466618f67495d49543ae78c1aff9
nickross <rossnc@gmail.com>
2024-04-17 16:11:53 -0500

    Minor Lint Fixes

--- src/cocoa/spring2024.py
+++ src/cocoa/spring2024.py
@@ -1,22 +1,19 @@
-
-import os
-import subprocess
-
-from repo import clone_repo
 from evaluate_repo import evaluate_repo
+from repo import clone_repo
 
 
 def process_repos(repo_urls):
     """
     Processes a list of Git repository URLs by cloning each repository, evaluating it,
-    and collecting the results. Each repository is cloned using the `clone_repo` function,
-    and then evaluated by calling the `evaluate_repo` function.
+    and collecting the results. Each repository is cloned using the `clone_repo`
+    function, and then evaluated by calling the `evaluate_repo` function.
 
     Parameters:
     - repo_urls (list of str): A list of URLs for the Git repositories to be processed.
 
     Returns:
-    - list of tuple: Each tuple contains the repository URL and the result of the evaluation.
+    - list of tuple: Each tuple contains the repository URL and the result of the
+            evaluation.
                       If the repository could not be processed, the result is `None`.
     """
     results = []
@@ -38,6 +35,6 @@ if __name__ == "__main__":
     # Example list of repositories to process
     repos = [
         "https://github.com/dsi-clinic/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper",
-        "https://github.com/dsi-clinic/cocoa.git"
+        "https://github.com/dsi-clinic/cocoa.git",
     ]
     process_repos(repos)
