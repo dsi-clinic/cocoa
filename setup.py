13135e7675b836642d0925fce1923d8d01e42bf7
chenhuifei01 <chenhui1@uchicago.edu>
2024-05-15 15:10:55 -0500

    test update the current pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cocoa",
-    version="0.0.1",
+    version="0.2.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
