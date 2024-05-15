a4eb3a9d5f8ec7f8c94a553940436990357c1d7a
chenhuifei01 <chenhui1@uchicago.edu>
2024-05-15 15:26:57 -0500

    test auto update pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cocoa",
-    version="0.2.3",
+    version="0.3.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
