8cc8e8314bb6bf57b8bb3b18caaa4c48fa7b0703
chenhuifei01 <chenhui1@uchicago.edu>
2024-05-15 14:56:08 -0500

    pre commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='cocoa',
-    version='0.0.1',
+    name="cocoa",
+    version="0.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
