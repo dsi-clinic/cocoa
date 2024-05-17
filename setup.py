02d1baaafcce99fcb34fdb95b7b2d75329bb3e55
chenhuifei01 <chenhui1@uchicago.edu>
2024-05-17 16:17:32 +0000

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cocoa",
-    version="0.5.2",
+    version="0.5.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
