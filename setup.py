8b1effeec33b788c9a6aadaf58a8ba00d319788e
testuser <testuser@uchicago.edu>
2024-05-17 19:11:35 +0000

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cocoa",
-    version="0.4.3",
+    version="0.4.4",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
